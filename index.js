92bcafa26d87a61f5a98d9119cdd748bbd88e752
Miyaki <90682254+DutchMTC@users.noreply.github.com>
2025-03-17 20:11:59 +0100

    Revert "Added standdown exclusion command"
    This reverts commit 7a59696bd33fa0f35710570d790975adde3300e5.

--- index.js
+++ index.js
@@ -120,7 +120,7 @@ async function sendReminders() {
         const mentions = missingMessages.map(member => `<@${member.id}>`).join(' ');
         await thread.send(
             `❗ ${mentions} ❗` +
-            `\n❗ Please don't forget to do your stand-downs before the end of the day! ❗`
+            `❗ Please don't forget to do your stand-downs before the end of the day! ❗`
         );
     }
 }
